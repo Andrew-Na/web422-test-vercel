dbe391f71ecedf0ed00e20e479b62e77e16e0060
Andrew-Na <47693154+Andrew-Na@users.noreply.github.com>
2023-12-03 20:59:27 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/", async (req, res) => {
+
+    return res.status(200).json({
+      msg: "hello",
+    });
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
